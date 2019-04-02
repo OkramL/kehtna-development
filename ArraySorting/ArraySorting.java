9f4169ccc7340b9bb9e179135746d1501f357775
Marko <livental@gmail.com>
2019-04-02 18:40:28 +0300

    at home
    :)

--- ArraySorting/ArraySorting.java
+++ ArraySorting/ArraySorting.java
@@ -8,24 +8,34 @@
 import java.util.Random;
 
 public class ArraySorting{
-	static int min, max, howMany;	
+	static int min, max, howMany;
+	static String how;
 	
 	public static void main(String[] args) {
-		if(args.length == 3) {
+		if(args.length == 4) {
 			if(isInteger(args[0]) && isInteger(args[1]) && isInteger(args[2])) {
 				min = Integer.parseInt(args[0]);
 				max = Integer.parseInt(args[1]);
 				howMany = Integer.parseInt(args[2]);				
+				how = args[3];
 				int[] rndNumbers = new int[howMany];
+				System.out.print("Original: ");
 				for(int i = 0; i < howMany; i++){
 					rndNumbers[i] = getRandomBetween(min, max);
+					System.out.print(rndNumbers[i] + " ");
 				}
+				System.out.println();
+				System.out.print("Sorted:   ");
+				rndNumbers = bubbleSort(rndNumbers, how);
 				
+				for(int i = 0; i < rndNumbers.length; i++){
+					System.out.print(rndNumbers[i] + " ");
+				}
 			} else {
 				System.out.println("One or multiple parameter(s) is not integer.");	
 			}
 		} else {
-			System.out.println("Must be three integer numbers. MIN MAX HOWMANY");
+			System.out.println("Must be three integer numbers. MIN MAX HOWMANY and one string ASC/DESC");
 		}
 	}
 	
@@ -44,11 +54,31 @@ public class ArraySorting{
 		return rndNr;
 	}
 	
-	private static int[] sortArray(int[] rndNumbers) {
-		for(int i = 0; i < rndNumbers.length()-1; i++) {
-			for(int j = i+1; j = ) {
-				
+	/**
+	* https://www.tutorialspoint.com/java-program-to-implement-bubble-sort
+	*
+	*/
+	private static int[] bubbleSort(int[] arr, String howSort) {
+		int n = arr.length;
+		int temp = 0;
+
+		for(int i = 0; i < n; i++) {
+			for(int j = 1; j < (n-i); j++) {
+				if(howSort.equalsIgnoreCase("asc")) {
+					if(arr[j-1] > arr[j]) {
+						temp = arr[j-1];
+						arr[j-1] = arr[j];
+						arr[j] = temp;
+					}
+				} else if(howSort.equalsIgnoreCase("desc")) {
+					if(arr[j-1] < arr[j]) {
+						temp = arr[j-1];
+						arr[j-1] = arr[j];
+						arr[j] = temp;
+					}
+				}				
 			}
 		}
+		return arr;
 	}
 }
