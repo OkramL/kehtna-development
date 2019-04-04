e09cb03acbb4a4e18ad27747b1b0fe7458a3a4c5
Marko <livental@gmail.com>
2019-04-04 11:43:28 +0300

    Create NavalBattle4.java

---
+++ NavalBattle4/src/NavalBattle4.java
@@ -0,0 +1,16 @@
+public class NavalBattle4 implements Runnable {
+
+    public NavalBattle4() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+
+
+}
